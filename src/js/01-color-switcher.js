0818a32404071bb79abecbef5a7356a8a07f3704
Siracenco Serghei <siracencoserghei@gmail.com>
2023-02-28 18:53:58 +0100

    add first resolved exersice 'color-switcher' from hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]'); 
+const body = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function onStartBtnClick (event) {
+    startBtn.setAttribute('desabled', true);
+timerId = setInterval(bodyColorSwitch, 1000)
+};
+
+function bodyColorSwitch (){
+    body.style.backgroundColor = getRandomHexColor();
+};
+
+function onStopBtnClick (event) {
+clearInterval(timerId);
+startBtn.removeAttribute('desabled');
+};
