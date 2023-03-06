d820ff3b9ca8dfccc6fe7b2af5a6273067701aef
Siracenco Serghei <siracencoserghei@gmail.com>
2023-03-06 05:58:37 +0100

    add resolved third exersise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+    return new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }, delay);
+  });
+};
+
+     function onFormSubmit (event) {
+        event.preventDefault();
+const {delay, step, amount} = event.currentTarget;
+const delayValue = Number(delay.value.trim());
+const stepValue = Number(step.value.trim());
+const amountValue = Number(amount.value.trim());
+let totalDelay = 0;
+
+for (let i = 1; i <= amountValue; i++) {
+  totalDelay = delayValue + stepValue * (i-1); 
+  createPromise(i, totalDelay)
+  .then(resolve => Notify.success(resolve))
+  .catch(reject => Notify.failure(reject));
+};
+};
